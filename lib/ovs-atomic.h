3d554cb5bc214aea27da09dbbd6efc59a51b76ec
Hemant Agrawal <hemant.agrawal@nxp.com>
2026-05-01 12:14:23 +0530

    Revert "use portable atomic operation"
    This reverts commit faac162e3dd8a903529e25768b120d19eb833430.

--- lib/ovs-atomic.h
+++ lib/ovs-atomic.h
@@ -325,7 +325,7 @@
 #include "util.h"
 
 #define IN_OVS_ATOMIC_H
-    #if __CHECKER__ || defined(__aarch64__)
+    #if __CHECKER__
         /* sparse doesn't understand some GCC extensions we use. */
         #include "ovs-atomic-pthreads.h"
     #elif __clang__ &&  __has_extension(c_atomic)
